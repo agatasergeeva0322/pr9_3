be095c653245d57a6f11e3bf1f82052060ae4da0
agatasergeeva0322 <157108699+agatasergeeva0322@users.noreply.github.com>
2024-02-22 20:16:44 +0500

    Train.kt

--- Train.kt
+++ Train.kt
@@ -1,23 +1,47 @@
 package Trains
 
-open class Train (var station: String,
+open class Train (var start_station: String,
+                  var end_station: String,
                   var time : String,
                   var date: String)
 {
+    open fun Hello()
+    {
+        println("Здравствуйте, Пассажир!")
+    }
     open fun Input_Application ()
     {
         try
         {
+            print("Станция отправления: ")
+            start_station = readLine()!!.toString()
             print("Станция назначения: ")
-            station = readLine()!!.toString()
+            end_station = readLine()!!.toString()
             print("Время, в которое вы бы хотели отправиться: ")
             time = readLine()!!.toString()
             var proverka = time.toCharArray()
             if (proverka[0]!!.toString()!!.toInt()>=0 && proverka[0]!!.toString()!!.toInt()<3 && proverka[1]!!.toString()!!.toInt()>=0 && proverka[1]!!.toString()!!.toInt()<10)
             {
                 if (proverka[3]!!.toString()!!.toInt()>=0 && proverka[3]!!.toString()!!.toInt()<6 && proverka[4]!!.toString()!!.toInt()>=0 && proverka[4]!!.toString()!!.toInt()<10) {
-                    print("Дата отправления:: ")
+                    print("Дата отправления: ")
                     date = readLine()!!.toString()
+                    var proverka_dd = date.toCharArray()
+                        if (proverka_dd[0]!!.toString()!!.toInt()>=0 && proverka_dd[0]!!.toString()!!.toInt()<=3 && proverka_dd[1]!!.toString()!!.toInt()>=0 && proverka_dd[1]!!.toString()!!.toInt()<10)
+                        {
+                            if (proverka_dd[3]!!.toString()!!.toInt() >= 0 && proverka_dd[3]!!.toString()!!.toInt() < 2 && proverka_dd[4]!!.toString()!!.toInt() >= 0 && proverka_dd[4]!!.toString()!!.toInt() < 3)
+                            {
+                                if (proverka_dd[6]!!.toString()!!.toInt() == 2 && proverka_dd[7]!!.toString()!!.toInt() == 0 && proverka_dd[8]!!.toString()!!.toInt() >= 0 && proverka_dd[8]!!.toString()!!.toInt() < 3 && proverka_dd[9]!!.toString()!!.toInt() >= 0 && proverka_dd[9]!!.toString()!!.toInt() < 10)
+                                {
+                                    if (proverka_dd[8]!!.toString()!!.toInt() == 2 && proverka_dd[9]!!.toString()!!.toInt() <= 4)
+                                    {
+                                    }
+                                    else println("Вы неправильно ввели год!")
+                                }
+                                else println("Вы неправильно ввели год!")
+                            }
+                            else println("Вы неправильно ввели месяц!")
+                        }
+                        else println("Вы неправильно ввели день!")
                 }
                 else println("Вы неправильно ввели минуты")
             }
@@ -27,11 +51,6 @@ open class Train (var station: String,
         catch (e : Exception)
         {
             println ("Ошибка")
-        }
-
-        fun Output_Application()
-        {
-            println("танция назначения: ${station}\nЖелаемое время начало поездки: ${time}\nДата:${date}")
         }
     }
 }
